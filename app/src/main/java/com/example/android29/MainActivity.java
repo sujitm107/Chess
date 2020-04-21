8fb284e528d31686780b85fec402b94032c34ed5
Sujit <sujit.mollet@rutgers.edu>
2020-04-20 23:48:28 -0400

    second commit

--- app/src/main/java/com/example/android29/MainActivity.java
+++ app/src/main/java/com/example/android29/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //This is Rachana's Comment
+        //This is Sujit's Comment
     }
 }
