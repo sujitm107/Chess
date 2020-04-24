86fd23a8789b514e233ee4e27f242be140c06cf4
Sujit <sujit.mollet@rutgers.edu>
2020-04-24 01:30:01 -0400

    added board

--- app/src/main/java/com/example/android29/ChessMatchActivity.java
+++ app/src/main/java/com/example/android29/ChessMatchActivity.java
@@ -16,14 +16,9 @@ public class ChessMatchActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //activates the up arrow
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
--- app/src/main/java/com/example/android29/MainActivity.java
+++ app/src/main/java/com/example/android29/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.android29;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +21,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void playButtonPressed(View view){
-        Log.i("Message: ", "Play Button Pressed");
 
-
+        Toast.makeText(this, "Play Button Pressed", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, ChessMatchActivity.class);
+        startActivity(intent);
 
     }
 
